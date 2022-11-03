37f09c592e08a4b202497d1f913751b53b0ef1d1
Mahdi Haji Abdolvahab <mahdi_h_a@yahoo.com>
2022-11-03 12:14:44 +0100

    add test projects

---
+++ EffectoryAssignment.Infrastructure.Tests/QuestionnaireRepositoryTests.cs
@@ -0,0 +1,18 @@
+namespace EffectoryAssignment.Infrastructure.Tests
+{
+    public class QuestionnaireRepositoryTests
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
